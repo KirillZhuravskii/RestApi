cf4d4c59c2fa702007c76cdd1725d2a9b82e6b1e
Kirill Zhuravskii <zhuravskii2606@gmail.com>
2024-06-06 16:15:03 +0300

    routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WorkerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [WorkerController::class, 'index'])->name('welcome');
+Route::get('/worker/create', [WorkerController::class, 'create'])->name('worker.create');
+Route::post('/worker/store', [WorkerController::class, 'store'])->name('worker.store');
+Route::get('/worker/edit', [WorkerController::class, 'create'])->name('worker.edit');
+Route::patch('/worker/update', [WorkerController::class, 'update'])->name('worker.update');
+Route::delete('/worker/destroy', [WorkerController::class, 'destroy'])->name('worker.destroy');
