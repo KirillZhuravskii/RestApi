e31a9da8df1064e712c1966d78f2cb1a8e5562ca
Kirill Zhuravskii <zhuravskii2606@gmail.com>
2024-06-06 16:13:21 +0300

    Подготовка основных методов для реализации CRUD

--- app/Http/Controllers/WorkerController.php
+++ app/Http/Controllers/WorkerController.php
@@ -6,5 +6,33 @@ use Illuminate\Http\Request;
 
 class WorkerController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('welcome');
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
 }
