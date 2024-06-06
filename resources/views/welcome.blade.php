fbb22f165a3320eedeec016859a1caa92dcd210a
Kirill Zhuravskii <zhuravskii2606@gmail.com>
2024-06-06 15:47:57 +0300

    Подключение стилей и реализация шапки сайта

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,22 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ url('public/css/style.css') }}">
     <title>Главная страница</title>
 </head>
 <body>
-
+<header class="header">
+    <div class="container">
+        <div class="header__inner">
+            <ul class="menu">
+                <h3 class="menu-logo"><a href="" class="menu-logo__heading">Главная</a></h3>
+                <nav class="nav">
+                    <li class="menu-item"><a href="#" class="menu-link">Создать</a></li>
+                    <li class="menu-item"><a href="#" class="menu-link">Список</a></li>
+                </nav>
+            </ul>
+        </div>
+    </div>
+</header>
 </body>
 </html>
